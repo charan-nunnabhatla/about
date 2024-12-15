d134afd1596a18f6be7b1794017b3b92cb5b4269
charan <charan.reings125@gmail.com>
2024-12-15 12:57:12 +0530

    fixed typo

--- src/components/custom/projects.tsx
+++ src/components/custom/projects.tsx
@@ -4,19 +4,19 @@ import Tile from "./tile";
 export default function Projects() {
   return (
     <BlurFade delay={1} >
-      <div className="h-auto flex flex-col mx-auto  p-5">
-        <h1 className=" font-bold text-lg md:text-xl lg:text-2xl">
+      <div className="flex flex-col h-auto p-5 mx-auto">
+        <h1 className="text-lg font-bold md:text-xl lg:text-2xl">
           My Projects
         </h1>
-        <div className="flex flex-col justify-center items-center my-3">
+        <div className="flex flex-col items-center justify-center my-3">
           <Tile
-            title="Dotted"
+            title="Dotted Life"
             sub_title="Calculates The Total Life Spend In Dots"
             date="Jan 2024"
             link="https://charan-nunnabhatla.github.io/dotted-life/"
           />
           <Tile
-            title="Grocery"
+            title="Grocery Webapp"
             sub_title="The E-Commerce Web App"
             date="Mar 2024"
             link="https://charan-nunnabhatla.github.io/e-commerce/"
