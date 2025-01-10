4e9d56849802f8b2d2b55765427204efb6149724
Charan <charan.reings125@gmail.com>
2025-01-10 15:54:55 +0530

    add new projects

--- src/components/custom/projects.tsx
+++ src/components/custom/projects.tsx
@@ -3,7 +3,7 @@ import Tile from "./tile";
 
 export default function Projects() {
   return (
-    <BlurFade delay={1} >
+    <BlurFade delay={1}>
       <div className="flex flex-col h-auto p-5 mx-auto">
         <h1 className="text-lg font-bold md:text-xl lg:text-2xl">
           My Projects
@@ -12,14 +12,26 @@ export default function Projects() {
           <Tile
             title="Dotted Life"
             sub_title="Calculates The Total Life Spend In Dots"
-            date="Jan 2024"
+            date="June 2024"
             link="https://charan-nunnabhatla.github.io/dotted-life/"
           />
           <Tile
-            title="Grocery Website"
+            title="Grocery"
             sub_title="The E-Commerce Website"
-            date="Mar 2024"
+            date="Aug 2024"
             link="https://charan-nunnabhatla.github.io/e-commerce/"
+          />
+          <Tile
+            title="Portfolio"
+            sub_title="Personal About Page (this page)"
+            date="Jan 2025"
+            link="https://charan-nunnabhatla.github.io/about/"
+          />
+          <Tile
+            title="SMEC Student Services (Major Project)"
+            sub_title="Digitalizing The College Works"
+            date="2024"
+            link="https://github.com/charan-nunnabhatla/SMEC-Student-Services"
           />
         </div>
       </div>
