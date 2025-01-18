a6a13f3e5ea9036f639b774664938e8f2fd07008
Charan <charan.reings125@gmail.com>
2025-01-18 16:04:34 +0530

    added greeting animations

--- src/components/custom/hero.tsx
+++ src/components/custom/hero.tsx
@@ -1,4 +1,6 @@
+import Lottie from "react-lottie-player";
 import BlurFade from "../ui/blur-fade";
+import hiAnimation from "../../../public/hi-animation.json";
 
 export default function Hero() {
   return (
@@ -9,18 +11,14 @@ export default function Hero() {
         <div
           id="name"
           className="text-[1.5em] md:text-[3em] lg:text[3em] whitespace-nowrap m-4 font-bold bg-transparent text-slate-400">
-          Hi, I'm Charan 👋
+          Hi, I'm Charan
           <div
             id="intro"
             className="text-xs font-sans text-slate-300 font-bold whitespace-normal md:text-lg max-w-[30em]">
-              Full Stack Developer | Flutter & React Specialist 
+            Full Stack Developer | Flutter & React Specialist
           </div>
         </div>
-        <img
-          className="size-[7em] mx-5 rounded-[50%]"
-          src="/about/IMG_1614.png"
-          alt="profile picture"
-        />
+        <Lottie animationData={hiAnimation} play loop className="size-1/4 md:size-3/4"/>
       </div>
     </BlurFade>
   );
