463a748c667e2b2e4f2829f0d91906a73ea02dab
Charan <charan.reings125@gmail.com>
2025-01-18 16:05:16 +0530

    added nextjs to tech stack

--- src/components/custom/tech-stack.tsx
+++ src/components/custom/tech-stack.tsx
@@ -1,6 +1,6 @@
 import MiniTile from "./mini-tile";
 import BlurFade from "../ui/blur-fade";
-import { RiTailwindCssFill } from "react-icons/ri";
+import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io";
 import { SiExpress, SiFlask, SiTypescript } from "react-icons/si";
 import { FaFlutter } from "react-icons/fa6";
@@ -9,11 +9,12 @@ import { FaNodeJs, FaPython, FaReact } from "react-icons/fa";
 export default function TechStack() {
   return (
     <BlurFade delay={1}>
-      <div className="flex flex-col text-lg p-5 font-bold md:text-xl lg:text-2xl">
+      <div className="flex flex-col p-5 text-lg font-bold md:text-xl lg:text-2xl">
         <h1>Tech Stack</h1>
-        <div className="flex flex-wrap justify-center flex-row gap-2 select-none p-2 items-center my-3">
+        <div className="flex flex-row flex-wrap items-center justify-center gap-2 p-2 my-3 select-none">
           <MiniTile title="React" Icon={FaReact} />
           <MiniTile title="Flutter" Icon={FaFlutter} />
+          <MiniTile title="Next.js" Icon={RiNextjsFill} />
           <MiniTile title="Python" Icon={FaPython} />
           <MiniTile title="JavaScript" Icon={IoLogoJavascript} />
           <MiniTile title="Node.js" Icon={FaNodeJs} />
