08de56ee145821cc57cc1b94a98f82116982d8cc
Charan <charan.reings125@gmail.com>
2025-02-08 07:49:28 +0530

    updated meta data

--- src/components/custom/mini-tile.tsx
+++ src/components/custom/mini-tile.tsx
@@ -10,17 +10,13 @@ export default function MiniTile({ title, Icon }: miniCardTypes) {
     <div
       aria-label={title}
       title={title}
-      className=
-        "flex items-center justify-center h-auto px-2 py-1 text-xs text-white rounded w-fit bg-white/25 hover:bg-white/30"
-      >
+      className="flex items-center justify-center h-auto px-2 py-1 text-xs text-white rounded w-fit bg-white/25 hover:bg-white/30">
       <span>
         {Icon ? (
           <div className="pr-2">
             <Icon />
           </div>
-        ) : (
-          <></>
-        )}
+        ) : null}
       </span>
       {title}
     </div>
