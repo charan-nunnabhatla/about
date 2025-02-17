e3c86d851fef36a7899e7655f4550d6fd5e822f7
Charan <charan.reings125@gmail.com>
2025-02-17 18:41:16 +0530

    added mongoDB

--- src/components/custom/tech-stack.tsx
+++ src/components/custom/tech-stack.tsx
@@ -2,7 +2,7 @@ import MiniTile from "./mini-tile";
 import BlurFade from "../ui/blur-fade";
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiExpress, SiFlask, SiTypescript } from "react-icons/si";
+import { SiExpress, SiFlask, SiMongodb, SiTypescript } from "react-icons/si";
 import { FaFlutter } from "react-icons/fa6";
 import { FaNodeJs, FaPython, FaReact } from "react-icons/fa";
 
@@ -13,15 +13,16 @@ export default function TechStack() {
         <div className="flex flex-col p-5 text-lg font-bold md:text-xl lg:text-2xl">
           <h1>Tech Stack</h1>
           <div className="flex flex-row flex-wrap items-center justify-center gap-2 p-2 my-3 select-none">
+            <MiniTile title="MongoDB" Icon={SiMongodb} />
+            <MiniTile title="Express" Icon={SiExpress} />
             <MiniTile title="React" Icon={FaReact} />
+            <MiniTile title="Node.js" Icon={FaNodeJs} />
             <MiniTile title="Flutter" Icon={FaFlutter} />
+            <MiniTile title="JavaScript" Icon={IoLogoJavascript} />
             <MiniTile title="Next.js" Icon={RiNextjsFill} />
             <MiniTile title="Python" Icon={FaPython} />
-            <MiniTile title="JavaScript" Icon={IoLogoJavascript} />
-            <MiniTile title="Node.js" Icon={FaNodeJs} />
             <MiniTile title="TypeScript" Icon={SiTypescript} />
             <MiniTile title="Tailwindcss" Icon={RiTailwindCssFill} />
-            <MiniTile title="Express" Icon={SiExpress} />
             <MiniTile title="Flask" Icon={SiFlask} />
           </div>
         </div>
